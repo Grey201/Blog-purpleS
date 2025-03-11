de86180489fe9b625a2e2509770ece0795261a9a
Grey <lserg3@yandex.ru>
2025-03-11 11:30:38 +0300

    did button & adaptive

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ import Button from '@/components/Button/Button';
 import LikeIcon from '../components/CardCategory/like.svg';
 
 export default function Home() {
+  const amountCards = [1, 2, 3];
   return (
     <>
-      <Card image={CardIm}>Card text</Card>
+      {amountCards.map((card) => (
+        <Card image={CardIm} key={card}></Card>
+      ))}
+      {/* <Card image={CardIm}>Card text</Card> */}
       <Button appearance='ghost'>
         <LikeIcon />
       </Button>
